673920562b513c21caf7838e2ed866a683a40a78
senokentiy <steve04935@gmail.com>
2026-01-12 19:23:26 +0300

    a bit cleaner

--- main.c
+++ main.c
@@ -1,9 +1,107 @@
 
 #include <SDL2/SDL.h>
 
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_shape.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+typedef struct
+{
+    char *ptype;
+    int width;
+    int height;
+    int maxrgb;
+} ppm_header;
+
+
+int
+parse_header (FILE *fd, ppm_header *hdr)
+{
+    char buf[8];
+    fgets (buf, sizeof (buf), fd);                     // P-specifier -- skip
+    fscanf (fd, "%d %d\n", &hdr->width, &hdr->height); // dimensions
+    fgets (buf, sizeof (buf), fd); // maximum rgb value -- skip
+
+    if (!hdr->height || !hdr->width)
+    {
+        fprintf (stderr, "[x] unable to parse dimensions.");
+        fclose (fd);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int
+fill_rgb (SDL_Window *window, FILE *fd, ppm_header *hdr)
+{
+    SDL_Surface *surface = SDL_GetWindowSurface (window);
+
+    if (surface == NULL)
+    {
+        fprintf (stderr, "[x] unable to get window surface: '%s'\n",
+                 SDL_GetError ());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Rect pix = (SDL_Rect){ 0, 0, 1, 1 };
+    Uint32 color = 0;
+    Uint8 rgb[3];
+
+    for (int y = 0; y < hdr->height; y++)
+    {
+        for (int x = 0; x < hdr->width; x++)
+        {
+            pix.x = x;
+            pix.y = y;
+
+            rgb[0] = getc (fd);
+            rgb[1] = getc (fd);
+            rgb[2] = getc (fd);
+            // printf ("%d %d %d\n", rgb[0], rgb[1], rgb[2]);
+
+            color = SDL_MapRGB (surface->format, rgb[0], rgb[1], rgb[2]);
+
+            int res = SDL_FillRect (surface, &pix, color);
+
+            if (res != EXIT_SUCCESS)
+            {
+                fprintf (stderr, "[x] unable to set pixel.");
+                fclose (fd);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int
+setup_window (SDL_Window *window, ppm_header *hdr)
+{
+    if (SDL_Init (SDL_INIT_VIDEO) != EXIT_SUCCESS)
+    {
+        fprintf (stderr, "[x] init error: %s\n", SDL_GetError ());
+        return EXIT_FAILURE;
+    }
+
+    window = SDL_CreateWindow ("image viewer", SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED, 100, 100, 0);
+    if (window == NULL)
+    {
+        fprintf (stderr, "[x] window error: %s\n", SDL_GetError ());
+        return 1;
+    }
+
+    return EXIT_SUCCESS;
+}
 
 
 int
@@ -11,7 +109,7 @@ main (int argc, char **argv)
 {
     if (argc != 2)
     {
-        printf ("[x] usage: %s IMAGE\n", argv[0]);
+        fprintf (stderr, "[x] usage: %s IMAGE\n", argv[0]);
         return 1;
     }
 
@@ -20,69 +118,35 @@ main (int argc, char **argv)
 
     if (fd == NULL)
     {
-        printf ("[x] unable to open file '%s'\n", fname);
+        fprintf (stderr, "[x] unable to open file '%s'\n", fname);
         return EXIT_FAILURE;
     }
 
     // parse ppm header
-    int height = -1;
-    int width = -1;
+    ppm_header hdr = (ppm_header){ NULL, 0, 0, 0 };
+    parse_header (fd, &hdr);
 
-    char buf[8];
-    fgets (buf, sizeof (buf), fd);               // P-specifier -- skip
-    fscanf (fd, "%d %d\n", &width, &height);     // dimensions
-    fgets (buf, sizeof (buf), fd);               // maximum rgb value -- skip
-
-    if (height == -1 || width == -1)
+    // window setup
+    SDL_Window *window = NULL;
+    // setup_window (window, &hdr);
+    if (SDL_Init (SDL_INIT_VIDEO) != EXIT_SUCCESS)
     {
-        puts ("[x] unable to parse dimensions.");
-        fclose (fd);
+        fprintf (stderr, "[x] init error: %s\n", SDL_GetError ());
         return EXIT_FAILURE;
     }
 
-    // window setup
-    if (SDL_Init (SDL_INIT_VIDEO) != 0)
-    {
-        fprintf (stderr, "[x] init error: %s\n", SDL_GetError ());
-        return 1;
-    }
-
-    SDL_Window *window = NULL;
-
-    window = SDL_CreateWindow ("image viewer", SDL_WINDOWPOS_UNDEFINED,
-                               SDL_WINDOWPOS_UNDEFINED, width, height, 0);
-
+    window
+        = SDL_CreateWindow ("image viewer", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, hdr.width, hdr.height, 0);
     if (window == NULL)
     {
         fprintf (stderr, "[x] window error: %s\n", SDL_GetError ());
         return 1;
     }
 
-    SDL_Surface *surface = SDL_GetWindowSurface (window);
-
     // parsing rgb
     // filling window
-    int x = 0;
-    int y = 0;
-    SDL_Rect pix = (SDL_Rect){ x, y, 1, 1 };
-    Uint32 color = 0;
-    Uint8 rgb[3];
-
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            pix.x = x;
-            pix.y = y;
-
-            rgb[0] = getc (fd);
-            rgb[1] = getc (fd);
-            rgb[2] = getc (fd);
-            color = SDL_MapRGB (surface->format, rgb[0], rgb[1], rgb[2]);
-
-            SDL_FillRect (surface, &pix, color);
-        }
-    }
+    fill_rgb (window, fd, &hdr);
 
     SDL_UpdateWindowSurface (window);
 
